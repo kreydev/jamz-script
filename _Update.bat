7a00a340a25a93f772c10e9e9fe34c63c43776ae
QfaceDubz <quackface2@gmail.com>
2023-05-10 10:23:58 -0400

    add update message

--- _Update.bat
+++ _Update.bat
@@ -1,1 +1,4 @@
 git pull
+echo off
+echo If git was not found, go to installers, run the install-scoop file, then go to other-scoops and run InstallGit
+pause
---
+++ theme.bat
@@ -0,0 +1,2 @@
+powershell Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
+powershell .\wallpaper.ps1
